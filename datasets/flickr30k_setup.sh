bf15f4db91805650db1952d1758adb2e3b1d3d6c
YicongY <able1994@hotmail.com>
2018-11-25 18:18:39 -0600

    add flickr30k

--- datasets/flickr30k_setup.sh
+++ datasets/flickr30k_setup.sh
@@ -1,6 +1,6 @@
 echo "Remember to set flickr dataset under data/flickr30k/"
 echo "Building datasets json file for flickr30k"
-python flickr30k.py 
+python flickr30k.py
 # create custom valid and test data
 echo "Building vocab for flickr30k"
 python build_vocab.py --json ../data/flickr30k/buildvocab.json --file_path ../data/flickr30k/vocab.pkl
